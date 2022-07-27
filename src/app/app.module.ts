b52888f006d0a57d76522434afd36e1fc1d18ea0
Mohit Jain <mohit.edu7895@gmail.com>
2022-07-27 18:03:19 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { StoryComponent } from './story/story.component';
+import { DiaryComponent } from './diary/diary.component';
+import { IdeaComponent } from './idea/idea.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    StoryComponent,
+    DiaryComponent,
+    IdeaComponent
   ],
   imports: [
     BrowserModule,
